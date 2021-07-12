2a3abe25f5ff3dbab10d3b9f3c59003bc9649ea4
Héctor Bracho Mendoza <hbracho55@hotmail.com>
2021-07-12 11:49:32 -0400

    creacion de modulo de requerimiento

---
+++ src/main/java/com/hjbm/requerimientobackend/RequerimientoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hjbm.requerimientobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequerimientoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequerimientoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hjbm/requerimientobackend/RequerimientoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjbm.requerimientobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequerimientoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
